f5775431db3e13e50fec68a858e04f0db8310a1e
nicol-hawkins <nicolchawkins@gmail.com>
2020-05-05 16:23:03 -0700

    liGenerator function done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const form = document.querySelector('form')
+  const ul = document.querySelector('ul')
+  const button = document.querySelector('button')
+  const input = document.getElementById('item')
+
+  const liGenerator = (text) => {
+    const li = document.createElement('li');
+    li.textContent = text;
+    ul.appendChild(li);
+  };
+
   return (
     <div className="App">
       <div class="small-container">
